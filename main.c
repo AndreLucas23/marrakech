1f45ceba0548c84c5c9e955768e6638757519e59
Jonathas <jonathaspolos7@gmail.com>
2025-04-30 13:37:59 -0300

    feat: Adicionando função de adicionar tapetes.

--- main.c
+++ main.c
@@ -21,21 +21,33 @@ int main() {
     Tabuleiro *tabuleiro = criarTabuleiro(TAM);
     Assam *assam = criarAssam(tabuleiro);
     Jogador *jogadores = criarJogadores(numJogadores);
-    imprimirTabuleiro(tabuleiro, assam);
+    imprimirTabuleiro(tabuleiro, assam, 0);
 
     while (1) {
         imprimirMenu(jogadores, turno);
         girarAssam(assam, perguntarGiro(assam));
-        imprimirTabuleiro(tabuleiro, assam);
+        imprimirTabuleiro(tabuleiro, assam, 0);
         dado = rolarDado();
         printf("Quantidade de casas a saltar: %d\n", dado);
         avancarAssam(assam, dado);
-        imprimirTabuleiro(tabuleiro, assam);
+        imprimirTabuleiro(tabuleiro, assam, 1);
+        printf("Onde deseja colocar o tapete: [ 1 / 2 / 3 / 4 / 0 ]\n");
+        int posicaoTapete;
+        scanf("%d", &posicaoTapete);
+        printf("Selecione a direção para colocar o tapete:\n");
+        printf("1 -> ↑\n");
+        printf("2 -> →\n");
+        printf("3 -> ↓\n");
+        printf("4 -> ←\n");
+        int rotacaoTapete;
+        scanf("%d", &rotacaoTapete);
+        colocarTapete(assam, posicaoTapete, rotacaoTapete, jogadores, turno);
+        imprimirTabuleiro(tabuleiro, assam, 0);
         printf("Pressione ENTER para continuar...");
         scanf("%*c");
 
         turno = (turno == numJogadores - 1) ? 0 : turno++;
     }
-
+    
     return 0;
 }
